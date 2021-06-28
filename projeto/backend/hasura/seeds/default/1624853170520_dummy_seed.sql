4e4ddd1b6d77abd553908682d7ae632ab0672a14
Lucas Harada <lucasyharada@gmail.com>
2021-06-28 01:46:55 -0300

    update to hasura config v3

---
+++ projeto/backend/hasura/seeds/default/1624853170520_dummy_seed.sql
@@ -0,0 +1,15 @@
+SET check_function_bodies = false;
+INSERT INTO public.lab (id, name, description, location, created_at, updated_at) VALUES (1, 'Lab1', 'Lab para testes de armas nucleares', 'Prédio da Elétrica, 13° andar', '2021-06-28 03:52:36.175932+00', '2021-06-28 03:52:36.175932+00');
+INSERT INTO public.lab (id, name, description, location, created_at, updated_at) VALUES (2, 'Lab2', NULL, 'Prédio da Mecânica, 1° andar', '2021-06-28 03:52:36.175932+00', '2021-06-28 03:52:36.175932+00');
+INSERT INTO public.lab_station (id, name, created_at, updated_at, lab_id) VALUES (1, 'Bancada 1A', '2021-06-28 03:52:36.175932+00', '2021-06-28 03:52:36.175932+00', 1);
+INSERT INTO public.lab_station (id, name, created_at, updated_at, lab_id) VALUES (2, 'Bancada 2B', '2021-06-28 03:52:36.175932+00', '2021-06-28 03:52:36.175932+00', 1);
+INSERT INTO public.lab_station (id, name, created_at, updated_at, lab_id) VALUES (3, 'Estação X', '2021-06-28 03:52:36.175932+00', '2021-06-28 03:52:36.175932+00', 2);
+INSERT INTO public.lab_station (id, name, created_at, updated_at, lab_id) VALUES (4, 'Estação Y', '2021-06-28 03:52:36.175932+00', '2021-06-28 03:52:36.175932+00', 2);
+INSERT INTO public.smart_plug (id, name, created_at, updated_at, lab_station_id) VALUES (1, 'Tomada 1 - Computador', '2021-06-28 03:52:36.175932+00', '2021-06-28 03:52:36.175932+00', 1);
+INSERT INTO public.smart_plug (id, name, created_at, updated_at, lab_station_id) VALUES (2, 'Tomada 2 - Placa FPGA', '2021-06-28 03:52:36.175932+00', '2021-06-28 03:52:36.175932+00', 1);
+INSERT INTO public.smart_plug (id, name, created_at, updated_at, lab_station_id) VALUES (3, 'Tomada 3 - Laser', '2021-06-28 03:52:36.175932+00', '2021-06-28 03:52:36.175932+00', 2);
+INSERT INTO public.smart_plug (id, name, created_at, updated_at, lab_station_id) VALUES (4, 'Computador do professor', '2021-06-28 03:52:36.175932+00', '2021-06-28 03:52:36.175932+00', 3);
+INSERT INTO public.smart_plug (id, name, created_at, updated_at, lab_station_id) VALUES (5, 'Microonadas dos funcionários', '2021-06-28 03:52:36.175932+00', '2021-06-28 03:52:36.175932+00', 4);
+SELECT pg_catalog.setval('public.laboratory_id_seq', 1, false);
+SELECT pg_catalog.setval('public.smart_plug_id_seq', 1, false);
+SELECT pg_catalog.setval('public.workbench_id_seq', 1, false);
